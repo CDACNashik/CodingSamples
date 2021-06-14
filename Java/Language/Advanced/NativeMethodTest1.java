1de21aece9df92215250049c1f46648f0bd5d855
CDACNashik <83755887+CDACNashik@users.noreply.github.com>
2021-06-14 12:58:01 +0530

    Add files via upload

--- Java/Language/Advanced/NativeMethodTest1.java
+++ Java/Language/Advanced/NativeMethodTest1.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 class NativeMethodTest1 {
 
 	private native static long gcd(long first, long second);
+
+	private native static String reverseOf(String text);
+
+	private native static int squareAll(double[] values);
 
 	public static void main(String[] args) {
 		System.loadLibrary("nmtsup"); //on UNIX this translates to libnmtsup.so which will be searched in each directory specified in java.library.path
@@ -9,6 +15,18 @@ class NativeMethodTest1 {
 				long m = Long.parseLong(args[1]);
 				long n = Long.parseLong(args[2]);
 				System.out.println(gcd(m, n));
+				break;
+			case "reverse":
+				String msg = args[1].toLowerCase();
+				System.out.println(reverseOf(msg));
+				break;
+			case "square":
+				double[] values = Arrays.stream(args)
+							.skip(1)
+							.mapToDouble(Double::parseDouble)
+							.toArray();
+				squareAll(values);
+				Arrays.stream(values).forEach(System.out::println);
 				break;
 		}
 	}
--- Java/Language/Advanced/nmtsup.cpp
+++ Java/Language/Advanced/nmtsup.cpp
@@ -14,3 +14,41 @@ JNIEXPORT jlong JNICALL Java_NativeMethodTest1_gcd
 	return first;
 }
 
+JNIEXPORT jstring JNICALL Java_NativeMethodTest1_reverseOf
+  (JNIEnv* env, jclass, jstring text)
+{
+	jint n = env->GetStringLength(text);
+	jchar* buffer = new jchar[n];
+
+	env->GetStringRegion(text, 0, n, buffer);
+	for(jint i = 0, j = n - 1; i < j; ++i, j--)
+	{
+		jchar ic = buffer[i];
+		jchar jc = buffer[j];
+		buffer[i] = jc;
+		buffer[j] = ic;
+	}
+
+	jstring reversed = env->NewString(buffer, n);
+	delete[] buffer;
+
+	return reversed;
+
+}
+
+JNIEXPORT jint JNICALL Java_NativeMethodTest1_squareAll
+  (JNIEnv* env, jclass, jdoubleArray values)
+{
+	jint n = env->GetArrayLength(values);
+	jdouble* buffer = new jdouble[n];
+
+	env->GetDoubleArrayRegion(values, 0, n, buffer);
+	for(jint i = 0; i < n; ++i)
+		buffer[i] *= buffer[i];
+	env->SetDoubleArrayRegion(values, 0, n, buffer);
+	delete[] buffer;
+
+	return n;
+	
+}
+
