206c3732b1587c385473f5f628cd608b11ddc0fc
CDACNashik <83755887+CDACNashik@users.noreply.github.com>
2021-06-30 12:14:31 +0530

    Add files via upload

---
+++ Java/Applications/Web/JSPTest/src/ClockTag.java
@@ -0,0 +1,38 @@
+package classic.web.app;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.*;
+
+public class ClockTag implements SimpleTag {
+
+	private JspContext context;
+	private JspFragment body;
+	private JspTag parent;
+
+	public void setJspContext(JspContext value) { context = value; }
+	
+	public void setJspBody(JspFragment value) { body = value; }
+
+	public void setParent(JspTag value) { parent = value; }
+
+	public JspTag getParent() { return parent; }
+
+	public void doTag() throws JspException, IOException {
+		JspWriter out = context.getOut();
+		Date now = new Date();
+		out.print(formatter.format(now));
+	}
+
+	private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	public void setFormat(String value) {
+		formatter.applyPattern(value);
+	}
+
+
+
+}
+
