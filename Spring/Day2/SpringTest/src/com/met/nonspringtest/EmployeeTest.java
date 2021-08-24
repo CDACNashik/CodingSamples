5c41457f633e1516f24659f78c4b47ba3f556000
CDACNashik <83755887+CDACNashik@users.noreply.github.com>
2021-08-24 15:04:48 +0530

    Add files via upload

--- Spring/Day2/SpringTest/src/com/met/nonspringtest/EmployeeTest.java
+++ Spring/Day2/SpringTest/src/com/met/nonspringtest/EmployeeTest.java
@@ -1,1 +1,19 @@
+package com.met.nonspringtest;
 
+import com.met.model.Employee;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee();
+		//employee.setName("");
+		
+		System.out.println(emp);
+		
+		Employee emp2 = new Employee();
+		System.out.println(emp2);
+		
+	}
+	
+}
