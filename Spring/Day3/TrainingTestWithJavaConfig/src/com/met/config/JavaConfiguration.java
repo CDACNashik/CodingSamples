5e9c4433258491e2413eb9908760347cc0ca7db9
CDACNashik <83755887+CDACNashik@users.noreply.github.com>
2021-08-24 15:29:29 +0530

    Add files via upload

--- Spring/Day3/TrainingTestWithJavaConfig/src/com/met/config/JavaConfiguration.java
+++ Spring/Day3/TrainingTestWithJavaConfig/src/com/met/config/JavaConfiguration.java
@@ -1,1 +1,55 @@
+package com.met.config;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.met.model.JavaTrainer;
+import com.met.model.Trainer;
+
+@Configuration			//<beans
+@ComponentScan(basePackages="com.met.model")		//<context:component-scan base-package="com.met.model"></context:component-scan>
+public class JavaConfiguration {
+
+	
+	/*<bean id="javaTrainer" class="com.met.model.JavaTrainer">
+	<property name="name" value="Ram"></property>
+	<property name="subjects">
+		<list>
+			<value>OOPS</value>
+			<value>JDBC</value>
+			<value>Streams</value>
+			<value>Collections</value>
+		</list>
+	</property>
+</bean>*/
+	
+	
+	@Bean
+	public Trainer javaTrainer(){						//name of function is id="javaTrainer"
+		JavaTrainer javaTrainer = new JavaTrainer();
+		javaTrainer.setName("Ram");
+		
+		List<String> subjects = new LinkedList<>();
+		subjects.add("OOPS");
+		subjects.add("JDBC");
+		subjects.add("Streams");
+		subjects.add("Collections");
+		
+		javaTrainer.setSubjects(subjects);
+		
+		
+		return javaTrainer;
+		
+	}
+	
+	
+	
+	
+}
+
+
 
