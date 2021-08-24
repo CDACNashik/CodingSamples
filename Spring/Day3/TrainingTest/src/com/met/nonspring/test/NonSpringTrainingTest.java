4b8ae65285317ab23b692341a14d030b3c64729d
CDACNashik <83755887+CDACNashik@users.noreply.github.com>
2021-08-24 15:10:27 +0530

    Add files via upload

--- Spring/Day3/TrainingTest/src/com/met/nonspring/test/NonSpringTrainingTest.java
+++ Spring/Day3/TrainingTest/src/com/met/nonspring/test/NonSpringTrainingTest.java
@@ -1,1 +1,53 @@
+package com.met.nonspring.test;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.met.model.JavaTrainer;
+import com.met.model.MetSolutions;
+import com.met.model.TrainingWorkshop;
+
+public class NonSpringTrainingTest {
+
+	
+	public static void main(String[] args) {
+		
+		/*JavaTrainer javaTrainer = new JavaTrainer();
+		javaTrainer.setName("Ramesh");
+		
+		List<String> listSubjects = new LinkedList<>();
+		listSubjects.add("OOPS");
+		listSubjects.add("JDBC");
+		listSubjects.add("Streams");
+		
+		listSubjects.add("Collections");*/
+		
+		JavaTrainer javaTrainer1 = new JavaTrainer();
+		javaTrainer1.setName("Ram");
+		
+		List<String> listSubjects = new LinkedList<>();
+		listSubjects.add("OOPS");
+		listSubjects.add("JDBC");
+		listSubjects.add("Streams");
+		
+		listSubjects.add("Collections");
+		
+		javaTrainer1.setSubjects(listSubjects);
+		
+		
+		MetSolutions metSolutions = new MetSolutions(javaTrainer1);
+		//metSolutions.conductTraining();
+		
+		
+		TrainingWorkshop workshop = new TrainingWorkshop();
+		workshop.setTrainingCompany(metSolutions);
+
+		workshop.conductWorkshop();
+		
+		
+		
+		
+	}
+	
+	
+}
