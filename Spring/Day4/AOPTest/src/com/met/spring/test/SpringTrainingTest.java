798822dd732c3a14e136ca4ec67058eea2d39d21
CDACNashik <83755887+CDACNashik@users.noreply.github.com>
2021-08-31 11:32:54 +0530

    Add files via upload

--- Spring/Day4/AOPTest/src/com/met/spring/test/SpringTrainingTest.java
+++ Spring/Day4/AOPTest/src/com/met/spring/test/SpringTrainingTest.java
@@ -1,1 +1,52 @@
+package com.met.spring.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import com.met.config.JavaConfiguration;
+import com.met.model.JavaDotNetTrainer;
+import com.met.model.Trainer;
+import com.met.model.TrainingCompany;
+import com.met.model.Workshop;
+
+public class SpringTrainingTest {
+
+	public static void main(String[] args) {
+		
+		/*ApplicationContext context = new ClassPathXmlApplicationContext("training.xml");
+		context.close();*/
+		
+		//try(AbstractApplicationContext context = new ClassPathXmlApplicationContext("training.xml")){
+		
+		
+		try(AbstractApplicationContext context = new AnnotationConfigApplicationContext(JavaConfiguration.class)){
+			
+			System.out.println("*************************Spring container initialization completed *************");
+			
+			/*JavaTrainer javaTrainer = context.getBean("javaTrainer", JavaTrainer.class);
+			javaTrainer.train();*/
+			
+			Trainer javaTrainer = context.getBean("javaTrainer", Trainer.class);
+			
+			System.out.println("Bean with id javaTrainer " + javaTrainer.getClass());
+			
+			//javaTrainer.train();
+			
+			//TrainingCompany trainingCompany = context.getBean(TrainingCompany.class);
+			
+			TrainingCompany trainingCompany = context.getBean("metSolutions", TrainingCompany.class);
+			//trainingCompany.conductTraining();
+			
+			
+			Workshop trainingWorkshop = context.getBean(Workshop.class);
+			trainingWorkshop.conductWorkshop();
+			
+			JavaDotNetTrainer javaDotNetTrainer = context.getBean(JavaDotNetTrainer.class);
+			javaDotNetTrainer.train();
+			System.out.println("Bean with id javaDotNetTrainer " + javaDotNetTrainer.getClass());
+			
+		}
+	}
+	
+}
 
